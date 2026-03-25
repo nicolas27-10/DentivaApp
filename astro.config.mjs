5b0138acf4c14825f852fbe51a14d5b3a825f5d1
nicolas27-10 <nic.castilloc@duocuc.cl>
2026-03-25 02:41:36 -0300

    installs and configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import react from '@astrojs/react'; // 👈 ¡El cambio está aquí! (astrojs en lugar de astro)
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false,
   },
   output: 'server',
   adapter: netlify(),
 });
-
-// Prueba de adaptador de Netlify
